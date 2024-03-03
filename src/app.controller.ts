615c1a7b3d5a43b63bae2148022eb7272276f03b
KIMBEOBWOO <qjqdn1568@naver.com>
2024-03-03 17:23:29 +0900

    feat : health check and ping/pong endpoint added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,28 @@
 import { Controller, Get, HttpCode, HttpStatus } from '@nestjs/common';
+import {
+  HealthCheck,
+  HealthCheckService,
+  HttpHealthIndicator,
+} from '@nestjs/terminus';
 
 @Controller()
 export class AppController {
-  @Get()
+  constructor(
+    private health: HealthCheckService,
+    private http: HttpHealthIndicator,
+  ) {}
+
+  @Get('ping')
   @HttpCode(HttpStatus.OK)
-  getHello() {
-    return true;
+  ping() {
+    return 'pong';
+  }
+
+  @Get('health')
+  @HealthCheck()
+  check() {
+    return this.health.check([
+      () => this.http.pingCheck('Self', 'http://127.0.0.1:3000/ping'),
+    ]);
   }
 }
